ab1e9b1fd05fdcfcdac70696145e744416d14b30
Fred Goo <fredera92@gmail.com>
2020-08-06 14:32:47 +0800

    数据源可配置

--- urule-springboot/src/main/java/com/bstek/urule/springboot/config/DatasourceConfig.java
+++ urule-springboot/src/main/java/com/bstek/urule/springboot/config/DatasourceConfig.java
@@ -1,6 +1,7 @@
 package com.bstek.urule.springboot.config;
 
 import org.apache.commons.dbcp.BasicDataSource;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,33 +12,42 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class DatasourceConfig {
 
+    @Value("${urule.datasource.uruleDataSource.driver}")
+    private String uruleDriver;
+    @Value("${urule.datasource.uruleDataSource.url}")
+    private String uruleUrl;
+    @Value("${urule.datasource.uruleDataSource.username}")
+    private String uruleUsername;
+    @Value("${urule.datasource.uruleDataSource.password}")
+    private String urulePwd;
+
+    @Value("${urule.datasource.remoteDataSource.driver}")
+    private String remoteDriver;
+    @Value("${urule.datasource.remoteDataSource.url}")
+    private String remoteUrl;
+    @Value("${urule.datasource.remoteDataSource.username}")
+    private String remoteUsername;
+    @Value("${urule.datasource.remoteDataSource.password}")
+    private String remotePwd;
+
     @Bean
     public BasicDataSource uruleDataSource() {
         BasicDataSource basicDataSource = new BasicDataSource();
-        basicDataSource.setDriverClassName("com.mysql.jdbc.Driver");
-        basicDataSource.setUrl("jdbc:mysql://127.0.0.1:3306/urule?useUnicode=true&characterEncoding=utf8&zeroDateTimeBehavior=convertToNull&autoReconnect=true&useSSL=false");
-        basicDataSource.setUsername("root");
-        basicDataSource.setPassword("123");
+        basicDataSource.setDriverClassName(this.uruleDriver);
+        basicDataSource.setUrl(this.uruleUrl);
+        basicDataSource.setUsername(this.uruleUsername);
+        basicDataSource.setPassword(this.urulePwd);
         return basicDataSource;
     }
 
     @Bean
     public BasicDataSource remoteDataSource() {
         BasicDataSource basicDataSource = new BasicDataSource();
-        basicDataSource.setDriverClassName("com.mysql.jdbc.Driver");
-        basicDataSource.setUrl("jdbc:mysql://192.168.112.17:3306/urule?useUnicode=true&characterEncoding=utf8&zeroDateTimeBehavior=convertToNull&autoReconnect=true&useSSL=false");
-        basicDataSource.setUsername("robin");
-        basicDataSource.setPassword("robin");
+        basicDataSource.setDriverClassName(this.remoteDriver);
+        basicDataSource.setUrl(this.remoteUrl);
+        basicDataSource.setUsername(this.remoteUsername);
+        basicDataSource.setPassword(this.remotePwd);
         return basicDataSource;
     }
 
-//    @Bean
-//    public BasicDataSource remoteDataSource() {
-//        BasicDataSource basicDataSource = new BasicDataSource();
-//        basicDataSource.setDriverClassName("com.mysql.jdbc.Driver");
-//        basicDataSource.setUrl("jdbc:mysql://192.168.100.61:8066/urule?useUnicode=true&characterEncoding=utf8&zeroDateTimeBehavior=convertToNull&autoReconnect=true&useSSL=false");
-//        basicDataSource.setUsername("suhui");
-//        basicDataSource.setPassword("suhui666");
-//        return basicDataSource;
-//    }
 }
